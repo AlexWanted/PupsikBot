36bb99e24b014f9cc3fd561466518f0148ef81e1
AlexWanted <32243410+AlexWanted@users.noreply.github.com>
2017-09-26 00:08:58 +0300

    'Changes'

---
+++ update.bat
@@ -0,0 +1,9 @@
+@echo off
+heroku maintenance:on
+git init
+git add .
+git commit -m 'Changes'
+git push -u https://github.com/AlexWanted/PupsikBot
+git push heroku master
+heroku ps:scale web=1
+heroku open
